96fe8114dc3b26231ff38727ed4953cf41c3e447
Pietromuresu <muresupietro66@gmail.com>
2023-10-02 14:35:59 +0200

    Step 1

--- src/main/java/org/java/MainController.java
+++ src/main/java/org/java/MainController.java
@@ -1,5 +1,20 @@
 package org.java;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		String name = "Pietro Muresu";
+		
+		model.addAttribute("name", name);
+		
+		return "index";
+	}
+	
 }
